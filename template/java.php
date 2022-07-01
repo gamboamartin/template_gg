f5b919bb515194974aa3be2c8fa1fe88baed5c89
mgamboa <gamboa.martin@gmail.com>
2022-07-01 10:05:55 -0500

    v0.4.0 Se integra java a template

--- template/java.php
+++ template/java.php
@@ -1,42 +1,14 @@
 <?php use config\generales;
 use config\views; ?>
+<script src="<?php echo (new views())->url_assets; ?>node_modules/jquery/dist/jquery.js"></script>
+<script src="<?php echo (new views())->url_assets; ?>node_modules/jquery-ui-dist/jquery-ui.js"></script>
+<script src="<?php echo (new views())->url_assets; ?>node_modules/popper.js/dist/umd/popper.js" ></script>
+<script src="<?php echo (new views())->url_assets; ?>node_modules/bootstrap/dist/js/bootstrap.js"></script>
+<script src="<?php echo (new views())->url_assets; ?>node_modules/bootstrap-select/dist/js/bootstrap-select.js"></script>
+<script src='<?php echo (new views())->url_assets; ?>node_modules/html5-qrcode/minified/html5-qrcode.min.js'></script>
+<script type="text/javascript" src="<?php echo (new views())->url_assets; ?>node_modules/datatables.net/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="<?php echo (new views())->url_assets; ?>node_modules/google-charts/dist/loader.js"></script>
+<script type="text/javascript" src="<?php echo (new views())->url_assets; ?>https://cdn.zingchart.com/zingchart.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
 
-<!-- Start Jquery -->
-<script src="<?php echo (new views())->url_assets; ?>js/jquery-2.2.1.min.js"></script>
-<script src="<?php echo (new views())->url_assets; ?>libraries/jquery.mobile/jquery.mobile.custom.min.js"></script>
-<!-- End Jquery -->
-<!-- Start BOOTSTRAP -->
-<script src="<?php echo (new views())->url_assets; ?>libraries/tether/dist/js/tether.min.js"></script>
-<script src="<?php echo (new views())->url_assets; ?>libraries/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="<?php echo (new views())->url_assets; ?>js/bootstrap-select.min.js"></script>
-<script src="<?php echo (new views())->url_assets; ?>libraries/bootstrap-colorpicker-master/dist/js/bootstrap-colorpicker.min.js"></script>
-<!-- End Bootstrap -->
-<!-- Start Template files -->
-<script src="<?php echo (new views())->url_assets; ?>js/winter-flat.js"></script>
-<!-- End  Template files -->
-<!-- Start owl-carousel -->
-<script src="<?php echo (new views())->url_assets; ?>libraries/owl.carousel/owl.carousel.min.js"></script>
-<!-- End owl-carousel -->
-<!-- Start JS MAP  -->
-<script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyABFoveisZMRWysqm4eCzhf_Jx2Yka4jEg&libraries=weather,geometry,visualization,places,drawing" type="text/javascript">
-</script>
-<script type="text/javascript" src="<?php echo (new views())->url_assets; ?>js/map_infobox.js"></script>
-<script type="text/javascript" src="<?php echo (new views())->url_assets; ?>js/markerclusterer.js"></script>
-<script src="<?php echo (new views())->url_assets; ?>js/map.js" type="text/javascript"></script>
-<!-- End JS MAP  -->
-<!-- Start blueimp  -->
-<script src="<?php echo (new views())->url_assets; ?>js/blueimp-gallery.min.js" type="text/javascript"></script>
-<!-- End blueimp  -->
-<script src="<?php echo (new views())->url_assets; ?>js/modernizr.custom.js"></script>
-<!-- Start custom template style  -->
-<script src="<?php echo (new views())->url_assets; ?>js/custom_template_style.js" type="text/javascript"></script>
-<!-- End custom template style   -->
-
-<script src="<?php echo (new views())->url_assets; ?>libraries/footable-jquery/js/footable.js"></script>
-<script src="<?php echo (new views())->url_assets; ?>js/footable_init.js"></script>
-
-<script src="<?php echo (new views())->url_assets; ?>js/dpejes/dpe.js"></script>
-<script src="<?php echo (new views())->url_assets; ?>js/dpejes_custom.js"></script>
-<script src="<?php echo (new views())->url_assets; ?>js/dpejes_custom.js"></script>
-<script src="<?php echo (new views())->url_assets; ?>js/facebook.js" type="text/javascript"></script>
+<script src="https://use.fontawesome.com/7a065420bf.js"></script>
