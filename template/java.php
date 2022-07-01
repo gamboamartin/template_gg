2e08da8eced04d3b056babdac901d6f22c1bc329
mgamboa <gamboa.martin@gmail.com>
2022-07-01 13:56:09 -0500

    v0.14.0 se integran template base

--- template/java.php
+++ template/java.php
@@ -8,7 +8,7 @@ use config\views; ?>
 <script src='<?php echo (new views())->url_assets; ?>node_modules/html5-qrcode/minified/html5-qrcode.min.js'></script>
 <script type="text/javascript" src="<?php echo (new views())->url_assets; ?>node_modules/datatables.net/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="<?php echo (new views())->url_assets; ?>node_modules/google-charts/dist/loader.js"></script>
-<script type="text/javascript" src="<?php echo (new views())->url_assets; ?>https://cdn.zingchart.com/zingchart.min.js"></script>
+<script type="text/javascript" src="https://cdn.zingchart.com/zingchart.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
 
 <script src="https://use.fontawesome.com/7a065420bf.js"></script>
