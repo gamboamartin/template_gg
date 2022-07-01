4cbcf7250cd0524b79cd36b16829210d70481758
mgamboa <gamboa.martin@gmail.com>
2022-07-01 10:03:07 -0500

    v0.3.0 Se integra css a template

--- template/css.php
+++ template/css.php
@@ -1,31 +1,11 @@
 <?php use config\generales;
 use config\views; ?>
-<link rel="icon" href="<?php echo (new generales())->url_base; ?>img/icons/Icono-TIQUE.png" type="image/x-icon" />
-<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,800,700&amp;subset=latin,cyrillic' rel='stylesheet' type='text/css' />
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>libraries/font-awesome/css/font-awesome.min.css" />
-<!-- Start BOOTSTRAP -->
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>libraries/tether/dist/css/tether.min.css" />
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>libraries/bootstrap/dist/css/bootstrap.min.css" />
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/bootstrap-select.min.css" />
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>libraries/bootstrap-colorpicker-master/dist/css/bootstrap-colorpicker.min.css" />
-<!-- End Bootstrap -->
-<!-- Start Template files -->
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/winter-flat.css" />
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/custom.css" />
-<!-- End  Template files -->
-<!-- Start owl-carousel -->
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>libraries/owl.carousel/assets/owl.carousel.css" />
-<!-- End owl-carousel -->
-<!-- Start JS MAP  -->
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/map.css" />
-<!-- End JS MAP  -->
-<!-- Start blueimp  -->
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/blueimp-gallery.min.css" />
-<!-- End blueimp  -->
-<!-- Start custom template style  -->
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/custom_template_style.css" />
-<!-- End custom template style   -->
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>libraries/footable-jquery/css/footable.bootstrap.min.css" />
-<!-- Inicio template proyecto local  -->
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>css/proyecto.css" />
-<!-- End template proyecto local   -->
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>node_modules/jquery-ui-dist/jquery-ui.css">
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>node_modules/bootstrap/dist/css/bootstrap.css">
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>node_modules/bootstrap/dist/css/bootstrap-grid.css">
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>node_modules/bootstrap/dist/css/bootstrap-reboot.css">
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>node_modules/bootstrap-icons/font/bootstrap-icons.css">
+<link rel="stylesheet" href=<?php echo (new views())->url_assets; ?>"node_modules/bootstrap-select/dist/css/bootstrap-select.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>base.css">
+<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
