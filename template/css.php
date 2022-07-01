15a4a99616128f4d0287ca491a6c4198c2574cb3
mgamboa <gamboa.martin@gmail.com>
2022-07-01 10:33:33 -0500

    v0.10.0 se ajsuta base.css

--- template/css.php
+++ template/css.php
@@ -7,5 +7,7 @@ use config\views; ?>
 <link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>node_modules/bootstrap-icons/font/bootstrap-icons.css">
 <link rel="stylesheet" href=<?php echo (new views())->url_assets; ?>"node_modules/bootstrap-select/dist/css/bootstrap-select.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
-<link rel="stylesheet" href="<?php echo (new views())->url_assets; ?>base.php">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+<style type="text/css">
+<?php include (new generales())->path_base.'assets/base.php'; ?>
+</style>
